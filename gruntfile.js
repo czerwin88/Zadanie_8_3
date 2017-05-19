de1cd1af59933649d09b0d252fb5d9d1e8320d97
Czerwin <r.j.czerwinski@gmail.com>
2017-05-19 17:38:31 +0200

    Add JSHint and other tasks

--- gruntfile.js
+++ gruntfile.js
@@ -1,13 +1,50 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    sass: {
+    options: {
+  	  sourceMap: true
+    },
+    dist: {
+      files: {
+	    'css/style.css': 'sass/style.sass'
+      }
+    }
+  },
+    
     jshint: {
       all: ['js/*.js']
-    }
+    },
+
+    browserSync: {
+    bsFiles: {
+      src : ['./css/*.css', './index.html']
+    },
+    options: {
+      watchTask: true,
+      server: {
+        baseDir: "./"
+      }
+	},
+ },
+	
+  watch: {
+    scripts: {
+      files: ['sass/*.sass'],
+      tasks: ['sass'],
+      options: {
+        spawn: false,
+      }
+	}
+  }
+
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'sass', 'browserSync', 'watch']);
 
 };
